708bb0776ccca7a2a1530fcb03b6bb83dec6d9f7
Benjamin Thompson <bigmekben@gmail.com>
2021-04-11 14:32:33 -0400

    end of chapter 6

--- src/RayTracingInAWeekend/RayTracingInAWeekend/Main.cpp
+++ src/RayTracingInAWeekend/RayTracingInAWeekend/Main.cpp
@@ -1,22 +1,16 @@
+#include "rtweekend.h"
+
 #include "color.h"
-#include "ray.h"
-#include "vec3.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 #include <iostream>
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
-	vec3 oc = r.origin() - center;
-	auto a = dot(r.direction(), r.direction());
-	auto b = 2.0 * dot(oc, r.direction());
-	auto c = dot(oc, oc) - radius * radius;
-	auto discriminant = b * b - 4 * a * c;
-	return (discriminant > 0);
-}
-
-color ray_color(const ray& r) {
-	if (hit_sphere(point3(0, 0, -1), 0.5, r))
+color ray_color(const ray& r, const hittable& world) {
+	hit_record rec;
+	if (world.hit(r, 0, infinity, rec))
 	{
-		return color(1, 0, 0);
+		return 0.5 * (rec.normal + color(1,1,1));
 	}
 	vec3 unit_direction = unit_vector(r.direction());
 	auto t = 0.5 * (unit_direction.y() + 1.0);
@@ -31,6 +25,11 @@ int main() {
 	// 256 x 144 (runs quicker but smaller image)
 	const int image_width = 455;
 	const int image_height = 256;
+
+	// World
+	hittable_list world;
+	world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+	world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
 	// Camera
 	auto viewport_height = 2.0;
@@ -58,7 +57,7 @@ int main() {
 				origin,
 				lower_left_corner + u * horizontal + v * vertical - origin
 			);
-			color pixel_color = ray_color(r);
+			color pixel_color = ray_color(r, world);
 			write_color(std::cout, pixel_color);
 		}
 	}
